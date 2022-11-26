526dd6d2c57bec5ea5aebff78e6089365eba2d91
Ariel Young <ariyoung@pm.me>
2022-11-25 17:36:34 -0800

    Added testing utilities to TabHandler, assorted bug fixes

--- src/cmd_input/tab_handler.rs
+++ src/cmd_input/tab_handler.rs
@@ -11,9 +11,18 @@ impl TabHandler {
         let tmp = vec![];
         TabHandler {
             suggesters:  vec![],
-            last_input:  "".to_string(),
+            last_input:  "poqjkfdpqokjpioejq".to_string(),
             cached_iter: Box::new(tmp.into_iter().cycle()),
         }
+    }
+
+    #[cfg(test)]
+    pub fn set_suggesters(&mut self, suggesters: Vec<Box<dyn Suggester>>) {
+        self.suggesters = suggesters;
+    }
+
+    pub fn get_suggesters(&self) -> &Vec<Box<dyn Suggester>> {
+        &self.suggesters
     }
 
     /// Returns an `Option<String>` representing the value that the current token should be
@@ -21,12 +30,13 @@ impl TabHandler {
     ///
     pub fn get_suggestion(&mut self, prefix: &String) -> Option<String> {
         if prefix != &self.last_input {
-            let mut suggestions = self.suggesters.iter().fold(vec![], |mut acc, s| {
+            let mut suggestions = self.suggesters.iter_mut().fold(vec![], |mut acc, s| {
                 acc.append(&mut s.get_suggestions(prefix));
                 acc
             });
             suggestions.sort();
             self.cached_iter = Box::new(suggestions.into_iter().cycle());
+            self.last_input = prefix.clone();
         }
 
         self.cached_iter.next().map(|suggestion| suggestion.replacement)
